0e46b53c334ba4e46032ff1acaa9c15c23d72c62
Marin Atanasov <tyxla@abv.bg>
2015-05-25 22:35:46 +0300

    include favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Pianist</title>
 
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="images/favicon.ico" rel="icon" type="image/x-icon">
 
 	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jplayer/2.9.2/jplayer/jquery.jplayer.min.js"></script>
